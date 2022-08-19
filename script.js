18cfbe2f65e70a1d5645d3db0574ed52e6b871b9
Robert Ford <robert.ford1996@gmail.com>
2022-08-18 21:40:31 -0400

    adds seperate divs

--- script.js
+++ script.js
@@ -3,3 +3,28 @@
 function tabOpen(){
     window.open("https://disneyworld.disney.go.com/vacation-planning/", "_blank")
 }
+
+//event listener possibly? to click on an image to bump down to ___ section on webpage
+
+
+// $('html, body').animate({ scrollTop: 0 }, 'fast');
+// either or ^ v cr stack overflow https://stackoverflow.com/questions/4147112/how-to-jump-to-top-of-browser-page
+// $('html,body').scrollTop(0);
+
+// $("body").css("page-size", "150%")
+
+// const img = document.querySelector('img')
+// img.onclick = () => {
+//   console.log('clicked')
+// }
+
+
+// const btn = document.querySelector(".btn")
+// btn.addEventListener("click", function (evt) {
+//   console.log(evt)
+// })
+
+const btn = document.querySelector('a')
+btn.addEventListener("click", function(evt){
+    console.log(evt)
+})

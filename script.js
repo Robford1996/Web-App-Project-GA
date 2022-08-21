e2aef80abe968580a4d95bbce91ef10afe8e778f
Robert Ford <robert.ford1996@gmail.com>
2022-08-20 22:59:58 -0400

    gets api in JS

--- script.js
+++ script.js
@@ -4,6 +4,41 @@ function tabOpen(){
     window.open("https://disneyworld.disney.go.com/vacation-planning/", "_blank")
 }
 
+console.log("https://api.themeparks.wiki/v1/entity/waltdisneyworldresort/live")
+
+// console.log($.ajax("https://api.themeparks.wiki/v1/entity/waltdisneyworldresort/live&t=teppan_edo"))
+
+const URL = "https://api.themeparks.wiki/v1/entity/waltdisneyworldresort/live=";
+
+//Variables
+const $name = $("#name")
+const $entitytype = $("#entitytype")
+const $status = $("#status")
+const $showtimes = $("#showtimes")
+const $queue = $("#queue")
+const $input = $('input[type="text"]')
+const $form = $("form")
+
+$form.on("submit", handleGetData)
+
+function handleGetData(event){
+    event.preventDefault()
+    userInput = $input.val()
+
+$.ajax(URL+userInput).then(function(data){
+    console.log('Information Posted!')
+    console.log(data)
+    $name.text(data.Name)
+    $entitytype.text(data.Entitytype)
+    $status.text(data.Status)
+    $showtimes.text(data.Showtimes)
+    $queue.text(data.Queue)
+}, function(error){
+    console.log("we broke it")
+    console.log(error)
+})
+}
+
 //event listener possibly? to click on an image to bump down to ___ section on webpage
 
 
